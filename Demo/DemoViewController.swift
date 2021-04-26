78f24874ba21fd526635774f814d5dc3cfac4ece
ceomat <crandrianambinina@ingenosya.mg>
2021-04-26 16:59:44 +0300

    close bouton et visible Player

--- Demo/DemoViewController.swift
+++ Demo/DemoViewController.swift
@@ -23,6 +23,8 @@ class DemoViewController: /*UIViewController,UIScrollViewDelegate */ DigitekaPla
     private var lastContentOffset : CGPoint = .zero
     //Title
     var clickedButton: String?
+    var event:String = ""
+    
     
 
     @IBOutlet weak var constraintHeight: NSLayoutConstraint!
@@ -35,16 +37,17 @@ class DemoViewController: /*UIViewController,UIScrollViewDelegate */ DigitekaPla
         let paramURL = "https://www.20minutes.fr/arts-stars/television/2981275-20210222-marseillais-dubai-quand-ca-allait-tapais-poing-table-previent-jessica-aidi-bookeuse"
         
         let paramSRC = "pqvp3r"
-        let autoplay = "1"
+        //let autoplay = "1"
         
         let paramMDTK = "01132356"
         let paramZONE = "54"
         let paramGDPRCONSENTSTRING = "BOj8iv4Oj8iwYAHABAlxCS-AAAAnF7_______9______9uz_Ov_v_f__33e87_9v_l_7_-___u_-3zd4-_1vf99yfm1-7etr3tp_87ues2_Xur__59__3z3_9phPrsk89r633A"
         
-        print("click = ",clickedButton)
+        //print("click = ",clickedButton)
+        print ("==> ",event)
         
                 
-        affiche_webview(_view: contentWebView,position:clickedButton,paramURL : paramURL, paramSRC : paramSRC, autoplay : autoplay, paramMDTK : paramMDTK, paramZONE : paramZONE, paramGDPRCONSENTSTRING : paramGDPRCONSENTSTRING)
+        affiche_webview(_view: contentWebView,position:clickedButton,paramURL : paramURL, paramSRC : paramSRC, autoplay : event, paramMDTK : paramMDTK, paramZONE : paramZONE, paramGDPRCONSENTSTRING : paramGDPRCONSENTSTRING)
         
         
     
